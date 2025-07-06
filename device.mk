70e3eb0ffd9672ce16c37ed2df24f1e47bf27799
MondayNitro <87489540+MondayNitro@users.noreply.github.com>
2025-07-06 21:44:24 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -48,7 +48,7 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.1-impl-qti.recovery \
     bootctrl.holi.recovery
 
 # Update engine
