29eb5ef4a753bc00a98b6976278f2d60994b3785
MondayNitro <87489540+MondayNitro@users.noreply.github.com>
2025-07-07 10:43:49 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -61,12 +61,11 @@ PRODUCT_PACKAGES += \
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
     libxml2 \
-    vendor.display.config@1.0 \
     vendor.display.config@2.0
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock \
     fastbootd
 
 # Partitions
