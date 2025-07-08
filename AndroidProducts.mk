bc8cd378aa44a6d0052c0a4b280a213b03436ed5
MondayNitro <87489540+MondayNitro@users.noreply.github.com>
2025-07-08 17:50:21 +0530

    efkjerkj

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,7 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_fogos.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_fogos-user \
-	twrp_fogos-userdebug \
-	twrp_fogos-eng \
-	
+    twrp_fogos-eng \
+	twrp_fogos-user \
+	twrp_fogos-userdebug
